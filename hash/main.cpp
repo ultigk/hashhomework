04e182e4ee9e9df955ff09379fb6d0621383a21d
ultigk <ultigk@mail.ru>
2019-04-23 00:38:03 +0300

    +trimword

--- hash/main.cpp
+++ hash/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <cmath>
 #include <fstream>
 #include <string>
 #include "Hash.h"
+
+/*std::string TrimWord(const std::string& raw_word)
+{
+    size_t begin_pos = 0;
+    size_t word_length = raw_word.npos;
+
+    for (size_t i = 0; i < raw_word.size(); i++)
+    {
+        if (isalpha(raw_word[i]))
+        {
+            begin_pos = i;
+            break;
+        }
+    }
+
+    for (size_t i = begin_pos; i < raw_word.size(); i++)
+    {
+        if (!isalpha(raw_word[i]))
+        {
+            word_length = i - begin_pos;
+            break;
+        }
+    }
+
+    return raw_word.substr(begin_pos, word_length);
+}*/
 
 int main()
 {
@@ -19,7 +46,7 @@ int main()
     std::string string;
     while (in_stream >> string)
     {
-        Hash.Add(string);
+        Hash.Add(/*TrimWord(*/string);
     }
 
     std::cout << "Otladka3" << std::endl;
